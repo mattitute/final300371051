1f26abfe0d287d1cf3be3be2fb548545d7f8bb2e
mattitute <tinaphat@gmail.com>
2024-08-10 12:35:02 -0700

    Create files

---
+++ src/main/java/org/example/final300371051/Final300371051Application.java
@@ -0,0 +1,13 @@
+package org.example.final300371051;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Final300371051Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Final300371051Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/final300371051/Final300371051ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.final300371051;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Final300371051ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
